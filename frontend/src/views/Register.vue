6acb5df9d36412abf650381cb48a5826d02f9fbb
elidondemolli <elidondemolli@gmail.com>
2021-07-19 11:44:41 +0200

    design improvement @ register page

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container py-5 mt-5">
+  <!-- <div class="container py-5 mt-5">
     <h1>LOGIN</h1>
     <form method="POST" @submit.prevent="login">
       <input v-model="name" placeholder="name" />
@@ -13,7 +13,37 @@
       <br />
       <button type="submit">Login</button>
     </form>
+  </div> -->
+  <div class="container py-5 mt-5" style="
+    padding-top: 190px !important;
+    width: 60%;">
+  <div class="row content">
+    <div class="col-md-6 mb-3">
+    </div>
+    <div class="col-md-6">
+      <h3 class="signin-text mb-3"> Sign In</h3>
+      <form method="POST" @submit.prevent="login">
+        <div class="form-group">
+          <label for="email">Name</label>
+          <input type="text" v-model="name" name="name" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" v-model="email" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" v-model="password" name="password" class="form-control">
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" name="checkbox" class="form-check-input" id="checkbox">
+          <label class="form-check-label" for="checkbox">Remember Me</label>
+        </div>
+        <button type="submit" class="btn btn-class">Login</button>
+      </form>
+    </div>
   </div>
+</div>
 </template>
 <script>
 import { register } from "../api/posts";
