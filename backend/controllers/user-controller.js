063f9af526b26835f1ad549efa97c234b756c60d
elidondemolli <elidondemolli@gmail.com>
2021-07-22 13:52:40 +0200

    backend logic for forgot password

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,12 +1,13 @@
 const User = require('../models/user');
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const getUsers = async (req, res) => {
     try {
         const result = await User.findById(req.user.id).select('-password');
-        res.status(200).json(result); 
+        res.status(200).json(result);
     } catch (error) {
         res.status(500).json({ message: error });
     }
@@ -14,18 +15,18 @@ const getUsers = async (req, res) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body;
-    
+
     try {
         let user = await User.findOne({ email: email });
 
-        if(!user){
-            return res.status(400).json({ message: 'USER DOES NOT EXIST'});
+        if (!user) {
+            return res.status(400).json({ message: 'USER DOES NOT EXIST' });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
 
-        if(!isMatch) {
-            return res.status(400).json({ message: 'Invalid Credentials'});
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid Credentials' });
         }
 
         const payload = {
@@ -50,12 +51,12 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
-    
+
     try {
         let user = await User.findOne({ email: email });
 
-        if(user){
-            return res.status(400).json({ message: 'USER ALREADY EXISTS'});
+        if (user) {
+            return res.status(400).json({ message: 'USER ALREADY EXISTS' });
         }
 
         user = new User({ name, email, password });
@@ -74,7 +75,7 @@ const register = async (req, res) => {
         const jwtToken = jwt.sign(payload, process.env.JWT_KEY, {
             expiresIn: '2h'
         });
-        
+
         await user.save();
 
         return res.status(200).json({ jwtToken });
@@ -85,4 +86,71 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = { getUsers, login, register }
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email: email })
+    if (!user) {
+        return res.status(400).json({ message: "User with this email doesnt exist." })
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_FORGOT_PASSWORD_KEY, {
+        expiresIn: '20m'
+    });
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.user,
+            pass: process.env.pass
+        }
+    });
+
+    let mailOptions = {
+        from: 'neuerm878@gmail.com',
+        to: email,
+        subject: 'Express Forgot Password Account',
+        html: `<h2>Please click on given link to proceed with our forgot password procedure</h2>
+               <a href="http://localhost:8080/reset-password/${token}">http://express.com/forgot-password</a>`,
+    };
+
+    try {
+        const result = await user.updateOne({ resetToken: token });
+        if (result) {
+            transporter.sendMail(mailOptions, function (err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Forgot Password Email Sent!');
+                }
+            });
+            return res.status(200).json({ message: 'sent' });
+        }
+    } catch (error) {
+        return res.status(400).json({ message: error });
+    }
+
+}
+
+const resetPassword = async (req, res) => {
+    const resetToken = req.params.token;
+
+    const salt = await bcrypt.genSalt(10);
+    req.body.newPass = await bcrypt.hash(req.body.newPass, salt);
+    try {
+        if (!resetToken) {
+            return res.status(403).json({ message: 'Token invalid or expired.' });
+        }
+
+        const verifyToken = jwt.verify(resetToken, process.env.JWT_FORGOT_PASSWORD_KEY);
+        if (verifyToken) {
+            await User.findOneAndUpdate({ resetToken: resetToken }, { password: req.body.newPass, resetToken: null });
+            return res.status(200).json({ message: 'Password has been updated' });
+        }
+    } catch (error) {
+        return res.status(400).json({ message: error });
+    }
+
+}
+
+module.exports = { getUsers, login, register, forgotPassword, resetPassword }
--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsers, login, register } = require('../controllers/user-controller')
+const { getUsers, login, register, forgotPassword, resetPassword } = require('../controllers/user-controller')
 
 router.get('/', getUsers);
 router.post('/login', login);
 router.post('/register', register);
+router.put('/forgot-password', forgotPassword);
+router.put('/reset-password/:token', resetPassword);
 
 module.exports = router
